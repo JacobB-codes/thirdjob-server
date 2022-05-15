d799428acd9540ad477c067cb4458ea25d6674a5
Jacob Burgess <jacobburgess11@gmail.com>
2022-05-15 00:03:08 -0700

    cookie settings

--- src/api/index.ts
+++ src/api/index.ts
@@ -70,7 +70,7 @@ app.use(
     }),
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
-      httpOnly: true, // browser not able to access session
+      httpOnly: false, // browser not able to access session
       sameSite: "lax", // csrf
       secure: __prod__, // cookie only works in https
       domain: __prod__ ? ".vercel.app" : undefined,
