c1c1f94f179f00379d114707ddc9e8827a2f2962
Jacob Burgess <jacobburgess11@gmail.com>
2022-05-14 22:52:56 -0700

    remove cookie domain specifier

--- src/api/index.ts
+++ src/api/index.ts
@@ -73,7 +73,6 @@ app.use(
       httpOnly: true, // browser not able to access session
       sameSite: "lax", // csrf
       secure: __prod__, // cookie only works in https
-      domain: __prod__ ? ".vercel.app" : undefined,
     },
     saveUninitialized: false, // dont store empty sessions
     secret: process.env.SESSION_SECRET!,
