3a0856a77eeae52b34bf1a909a60cfc214d24182
Jacob Burgess <jacobburgess11@gmail.com>
2022-05-14 23:40:07 -0700

    cookie domain setting

--- src/api/index.ts
+++ src/api/index.ts
@@ -73,6 +73,7 @@ app.use(
       httpOnly: true, // browser not able to access session
       sameSite: "lax", // csrf
       secure: __prod__, // cookie only works in https
+      domain: __prod__ ? ".vercel.app" : undefined,
     },
     saveUninitialized: false, // dont store empty sessions
     secret: process.env.SESSION_SECRET!,
